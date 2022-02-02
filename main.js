c7097e19441f719551ea22a05b7ea26be4d7cb25
krambambula <39031685+krambambula@users.noreply.github.com>
2022-02-02 19:40:04 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     console = new console();
     board = new board();
     console.printLine("test");
-    board.generate(30,20);
+    board.generate(10,10);
     board.place("@", 4, 4);
     board.placePlayer();
 });
